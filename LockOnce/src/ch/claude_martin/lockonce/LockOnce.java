e4dfeca1a60352a1c99fa0c0e8b0280576c829f1
claudemartin <claude.martin@students.fhnw.ch>
2013-07-10 21:01:37 +0200

    Just some cleaning up (more conventional formatter).

--- LockOnce/src/ch/claude_martin/lockonce/LockOnce.java
+++ LockOnce/src/ch/claude_martin/lockonce/LockOnce.java
@@ -3,125 +3,161 @@ package ch.claude_martin.lockonce;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-/** The <code>LockOnce</code> class represents an object used to guard some block of code that is only to be accessed once.
+/**
+ * The <code>LockOnce</code> class represents an object used to guard some block
+ * of code that is only to be accessed once.
  * 
  * <p>
- * This class implements the <a href="http://en.wikipedia.org/wiki/Double-checked_locking">double-checked-locking</a> idiom and allows to
- * use it just like similar classes, such as the {@link ReentrantLock}.
+ * This class implements the <a
+ * href="http://en.wikipedia.org/wiki/Double-checked_locking"
+ * >double-checked-locking</a> idiom and allows to use it just like similar
+ * classes, such as the {@link ReentrantLock}.
  * 
  * <p>
- * This could be used for lazy singleton initialization. However, this is considered to be an antipattern by some. They argue that it is
- * better to use a nested static class as a <a href="http://en.wikipedia.org/wiki/Initialization_on_demand_holder_idiom"><i>initialization
- * on demand holder</i></a>. The argument that it is broken is only true for JVM &lt;= 1.4, but not for newer versions.
+ * This could be used for lazy singleton initialization. However, this is
+ * considered to be an antipattern by some. They argue that it is better to use
+ * a nested static class as a <a
+ * href="http://en.wikipedia.org/wiki/Initialization_on_demand_holder_idiom"
+ * ><i>initialization on demand holder</i></a>. The argument that it is broken
+ * is only true for JVM &lt;= 1.4, but not for newer versions.
  * 
  * <p>
- * The given solution in this class is safe on JVM 1.5+ thanks to the Java Memory Model. But fields of a created singleton are still not
- * safe unless final or volatile.
+ * The given solution in this class is safe on JVM 1.5+ thanks to the Java
+ * Memory Model. But fields of a created singleton are still not safe unless
+ * final or volatile.
  * 
  * <p>
- * It is recommended practice to <em>always</em> immediately follow a call to {@code lockOnce} with a {@code try} block, most typically in a
- * before/after construction such as:
+ * It is recommended practice to <em>always</em> immediately follow a call to
+ * {@code lockOnce} with a {@code try} block, most typically in a before/after
+ * construction such as: 
  * 
- * <pre>
- * <tt>
+ * <pre><tt>
  * final LockOnce lo = new LockOnce();
  * if (lo.lockOnce()) try {
  *   // ... method body
  * } finally {
  *   lo.unlock();
- * }</tt>
- * </pre>
+ * }</tt></pre>
  * 
- * @author Claude Martin */
+ * @author Claude Martin
+ */
 public class LockOnce {
-	/** Object was only created, no lock requests so far. */
-	private static final byte		STATE_UNTAPPED	= 0;
-	/** Currently locked. Some Thread is doing some critical work. It is supposed to call {@link #unlock()}. */
-	private static final byte		STATE_LOCKED		= 1;
-	/** This LockOnce is spent, all further calls to {@link #lockOnce()} will return <code>false</code> quickly. */
-	private static final byte		STATE_SPENT			= 2;
-	/** Current state. */
-	private volatile byte				state						= STATE_UNTAPPED;
-	/** Used ReentrantLock-Object. */
-	private final ReentrantLock	lock						= new ReentrantLock();
-	/** Condition used to wait for a call to {@link #unlock()}. */
-	private final Condition			unlocked				= this.lock.newCondition();
-	private Thread							thread					= null;
+  /** Object was only created, no lock requests so far. */
+  private static final byte STATE_UNTAPPED = 0;
+  /**
+   * Currently locked. Some Thread is doing some critical work. It is supposed
+   * to call {@link #unlock()}.
+   */
+  private static final byte STATE_LOCKED = 1;
+  /**
+   * This LockOnce is spent, all further calls to {@link #lockOnce()} will
+   * return <code>false</code> quickly.
+   */
+  private static final byte STATE_SPENT = 2;
+  /** Current state. */
+  private volatile byte state = STATE_UNTAPPED;
+  /** Used ReentrantLock-Object. */
+  private final ReentrantLock lock = new ReentrantLock();
+  /** Condition used to wait for a call to {@link #unlock()}. */
+  private final Condition unlocked = this.lock.newCondition();
+  private Thread thread = null;
 
-	/** Returns <code>true</code> on only one invocation. All others get <code>false</code>.
-	 * 
-	 * Note: It is not necessarily the first invocation that get's <code>true</code>. But other threads will block untill {@link #unlock()} is
-	 * called the first time. A returned <code>false</code> is guaranteed to be returned after the receiver of <code>true</code> has finished
-	 * and changes all are visible. */
-	public boolean lockOnce() {
-		try {
-			return this.lockOnce(false);
-		} catch (final InterruptedException e) {
-			// This never happens!
-			return false;
-		}
-	}
+  /**
+   * Returns <code>true</code> on only one invocation. All others get
+   * <code>false</code>.
+   * 
+   * Note: It is not necessarily the first invocation that get's
+   * <code>true</code>. But other threads will block until {@link #unlock()} is
+   * called the first time. A returned <code>false</code> is guaranteed to be
+   * returned after the receiver of <code>true</code> has finished and changes
+   * all are visible.
+   */
+  public boolean lockOnce() {
+    try {
+      return this.lockOnce(false);
+    } catch (final InterruptedException e) {
+      // This never happens!
+      return false;
+    }
+  }
 
-	/** Returns <code>true</code> on only one invocation unless the current thread is {@linkplain Thread#interrupt interrupted} .
-	 * 
-	 * @see #lockOnce() */
-	public boolean lockOnceInterruptibly() throws InterruptedException {
-		return this.lockOnce(true);
-	}
+  /**
+   * Returns <code>true</code> on only one invocation unless the current thread
+   * is {@linkplain Thread#interrupt interrupted} .
+   * 
+   * @see #lockOnce()
+   */
+  public boolean lockOnceInterruptibly() throws InterruptedException {
+    return this.lockOnce(true);
+  }
 
-	private boolean lockOnce(final boolean interruptably) throws InterruptedException {
-		if (this.state == STATE_SPENT) return false;
-		if (interruptably) this.lock.lockInterruptibly();
-		else this.lock.lock();
-		try {
-			while (this.state == STATE_LOCKED) {
-				if (this.thread == Thread.currentThread())
-					throw new IllegalMonitorStateException("Multiple calls by the same thread are not permitted.");
-				if (interruptably) this.unlocked.await();
-				else this.unlocked.awaitUninterruptibly();
-			}
-			if (this.state == STATE_SPENT) return false;
-			if (this.state == STATE_UNTAPPED)
-				this.state = STATE_LOCKED;
-			this.thread = Thread.currentThread();
-			return true;
-		} finally {
-			this.lock.unlock();
-		}
-	}
+  private boolean lockOnce(final boolean interruptably)
+      throws InterruptedException {
+    if (this.state == STATE_SPENT)
+      return false;
+    if (interruptably)
+      this.lock.lockInterruptibly();
+    else
+      this.lock.lock();
+    try {
+      while (this.state == STATE_LOCKED) {
+        if (this.thread == Thread.currentThread())
+          throw new IllegalMonitorStateException(
+              "Multiple calls by the same thread are not permitted.");
+        if (interruptably)
+          this.unlocked.await();
+        else
+          this.unlocked.awaitUninterruptibly();
+      }
+      if (this.state == STATE_SPENT)
+        return false;
+      if (this.state == STATE_UNTAPPED)
+        this.state = STATE_LOCKED;
+      this.thread = Thread.currentThread();
+      return true;
+    } finally {
+      this.lock.unlock();
+    }
+  }
 
-	/** Unlock the LockOnce mechanism. This should be called from a finally-block.
-	 * 
-	 * <p>
-	 * Invocation from the wrong thread or before any call to {@link #lockOnce()} results in a {@link IllegalMonitorStateException}. */
-	public void unlock() {
-		if (this.state == STATE_UNTAPPED)
-			throw new IllegalMonitorStateException("unlock() can not be called if lockOnce() was never called before.");
-		if (this.thread != Thread.currentThread())
-			throw new IllegalMonitorStateException("This thread does not hold the lock.");
-		if (this.state == STATE_SPENT) return;
-		this.lock.lock();
-		try {
-			if (this.state == STATE_SPENT) return;
-			this.state = STATE_SPENT;
-			this.unlocked.signalAll();
-		} finally {
-			this.lock.unlock();
-		}
-	}
+  /**
+   * Unlock the LockOnce mechanism. This should be called from a finally-block.
+   * 
+   * <p>
+   * Invocation from the wrong thread or before any call to {@link #lockOnce()}
+   * results in a {@link IllegalMonitorStateException}.
+   */
+  public void unlock() {
+    if (this.state == STATE_UNTAPPED)
+      throw new IllegalMonitorStateException(
+          "unlock() can not be called if lockOnce() was never called before.");
+    if (this.thread != Thread.currentThread())
+      throw new IllegalMonitorStateException(
+          "This thread does not hold the lock.");
+    if (this.state == STATE_SPENT)
+      return;
+    this.lock.lock();
+    try {
+      if (this.state == STATE_SPENT)
+        return;
+      this.state = STATE_SPENT;
+      this.unlocked.signalAll();
+    } finally {
+      this.lock.unlock();
+    }
+  }
 
-	@Override
-	public String toString() {
-		String strState;
-		if (this.state == STATE_LOCKED) {
-			strState = "locked";
-		} else if (this.state == STATE_SPENT) {
-			strState = "spent";
-		} else if (this.state == STATE_UNTAPPED) {
-			strState = "untapped";
-		} else {
-			strState = "faulty";
-		}
-		return "LockOnce[" + strState + "]";
-	}
+  @Override
+  public String toString() {
+    String strState;
+    if (this.state == STATE_LOCKED)
+      strState = "locked";
+    else if (this.state == STATE_SPENT)
+      strState = "spent";
+    else if (this.state == STATE_UNTAPPED)
+      strState = "untapped";
+    else
+      strState = "faulty";
+    return "LockOnce[" + strState + "]";
+  }
 }
--- LockOnce/src/ch/claude_martin/lockonce/LockOnceTest.java
+++ LockOnce/src/ch/claude_martin/lockonce/LockOnceTest.java
@@ -6,189 +6,205 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LockOnceTest {
-	// any good code holds this constant:
-	static final int								THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING	= 42;
+  // any good code holds this constant:
+  static final int THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING = 42;
 
-	// This does not even need to be volatile. Changes are visible on access thanks to JMM.
-	// Can't begin at 0, because that would just be the default value.
-	static int											check																						= THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING;
+  // This does not even need to be volatile. Changes are visible on access
+  // thanks to JMM.
+  // Can't begin at 0, because that would just be the default value.
+  static int check = THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING;
 
-	// a singleton is still be reccommended to declared this as volatile!
-	static volatile PseudoSingleton	singleton	= null;
+  // a singleton is still be recommended to declared this as volatile!
+  static volatile PseudoSingleton singleton = null;
 
-	@Test
-	public void loops() throws Throwable {
-		final int loops = 20;
-		for (int i = 0; i < loops; i++) {
-			final int expected = check + 1;
-			final LockOnce lo = new LockOnce();
-			final int threads = 20;
-			final ExecutorService threadPool = Executors.newFixedThreadPool(threads);
-			final AtomicReference<Throwable> exceptionThrown = new AtomicReference<>(null);
-			final AtomicBoolean singletonIsPoppulated = new AtomicBoolean(false);
-			// runnable1 doesn't really test the code but the JVM, which usually is not the idea of JUnit tests.
-			// But this is just to show how this really is thread safe.
-			final Runnable runnable1 = new Runnable() {
-				@Override
-				public void run() {
-					try {
-						for (int i = 0; singleton == null || !singletonIsPoppulated.get() && i++ < 100;)
-							Thread.sleep(100);
-						// access to the fake "singleton" without using LockOnce:
-						Assert.assertTrue("runnable1: Wrong value (foo1)", singleton.foo1 != 0);
-						Assert.assertTrue("runnable1: Wrong value (foo2)", singleton.foo2 != 0);
-						Assert.assertTrue("runnable1: Wrong value (foo3)", singleton.getFoo3() != 0);
-					} catch (final Throwable e) {
-						exceptionThrown.set(e);
-					}
-				}
-			};
-			// runnable2 actually tests lockOnce():
-			final Runnable runnable2 = new Runnable() {
-				@Override
-				public void run() {
-					try {
-						for (int j = 0; j < 10; j++)
-							lo.toString(); // Just to keep the Threads busy.
-						if (lo.lockOnce()) {
-							try {
-								final int copy = check;
-								singleton = new PseudoSingleton(check + 1);
-								Thread.sleep(50);
-								singleton.foo2 = check + 1;
-								Thread.sleep(50);
-								singleton.setFoo3(check + 1);
-								singletonIsPoppulated.set(true);
-								check = copy + 1;
-							} finally {
-								lo.unlock();
-							}
-						}
-						// value must be correct in any case:
-						Assert.assertEquals("runnable2: Wrong value (check)", expected, check);
-						Assert.assertEquals("runnable2: Wrong value (foo1)", expected, singleton.foo1);
-						Assert.assertEquals("runnable2: Wrong value (foo2)", expected, singleton.foo2);
-						Assert.assertEquals("runnable2: Wrong value (foo3)", expected, singleton.getFoo3());
-					} catch (final Throwable e) {
-						exceptionThrown.set(e);
-					}
-				}
-			};
+  @SuppressWarnings("static-method")
+  @Test
+  public void loops() throws Throwable {
+    final int loops = 20;
+    for (int i = 0; i < loops; i++) {
+      final int expected = check + 1;
+      final LockOnce lo = new LockOnce();
+      final int threads = 20;
+      final ExecutorService threadPool = Executors.newFixedThreadPool(threads);
+      final AtomicReference<Throwable> exceptionThrown = new AtomicReference<>(
+          null);
+      final AtomicBoolean singletonIsPoppulated = new AtomicBoolean(false);
+      // runnable1 doesn't really test the code but the JVM, which usually is
+      // not the idea of JUnit tests.
+      // But this is just to show how this really is thread safe.
+      final Runnable runnable1 = new Runnable() {
+        @Override
+        public void run() {
+          try {
+            for (int j = 0; singleton == null || !singletonIsPoppulated.get()
+                && j++ < 100;)
+              Thread.sleep(100);
+            // access to the fake "singleton" without using LockOnce:
+            Assert.assertTrue("runnable1: Wrong value (foo1)",
+                singleton.foo1 != 0);
+            Assert.assertTrue("runnable1: Wrong value (foo2)",
+                singleton.foo2 != 0);
+            Assert.assertTrue("runnable1: Wrong value (foo3)",
+                singleton.getFoo3() != 0);
+          } catch (final Throwable e) {
+            exceptionThrown.set(e);
+          }
+        }
+      };
+      // runnable2 actually tests lockOnce():
+      final Runnable runnable2 = new Runnable() {
+        @Override
+        public void run() {
+          try {
+            for (int j = 0; j < 10; j++)
+              lo.toString(); // Just to keep the Threads busy.
+            if (lo.lockOnce())
+              try {
+                final int copy = check;
+                singleton = new PseudoSingleton(check + 1);
+                Thread.sleep(50);
+                singleton.foo2 = check + 1;
+                Thread.sleep(50);
+                singleton.setFoo3(check + 1);
+                singletonIsPoppulated.set(true);
+                check = copy + 1;
+              } finally {
+                lo.unlock();
+              }
+            // value must be correct in any case:
+            Assert.assertEquals("runnable2: Wrong value (check)", expected,
+                check);
+            Assert.assertEquals("runnable2: Wrong value (foo1)", expected,
+                singleton.foo1);
+            Assert.assertEquals("runnable2: Wrong value (foo2)", expected,
+                singleton.foo2);
+            Assert.assertEquals("runnable2: Wrong value (foo3)", expected,
+                singleton.getFoo3());
+          } catch (final Throwable e) {
+            exceptionThrown.set(e);
+          }
+        }
+      };
 
-			threadPool.submit(runnable1);
-			threadPool.submit(runnable1);
-			for (int j = 0; j < threads; j++) {
-				threadPool.submit(runnable2);
-			}
-			threadPool.shutdown();
-			threadPool.awaitTermination(10, TimeUnit.SECONDS);
-			if (exceptionThrown.get() != null)
-				throw exceptionThrown.get();
-			Assert.assertEquals("Wrong value", expected, check);
-			singleton = null;
-		}
+      threadPool.submit(runnable1);
+      threadPool.submit(runnable1);
+      for (int j = 0; j < threads; j++)
+        threadPool.submit(runnable2);
+      threadPool.shutdown();
+      threadPool.awaitTermination(10, TimeUnit.SECONDS);
+      if (exceptionThrown.get() != null)
+        throw exceptionThrown.get();
+      Assert.assertEquals("Wrong value", expected, check);
+      singleton = null;
+    }
 
-		Assert.assertEquals("Wrong total amount", loops + THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING, check);
-	}
+    Assert.assertEquals("Wrong total amount", loops
+        + THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING, check);
+  }
 
-	@Test
-	public void multipleCalls() throws InterruptedException {
-		try {
-			final LockOnce lo = new LockOnce();
-			Assert.assertTrue(lo.lockOnce()); // first one must be ok.
-			lo.lockOnce(); // second one makes no sense! it should fail quickly.
-			Assert.fail("Second call to LockOnce.lockOnce() did not throw any Exception :-(");
-		} catch (final IllegalMonitorStateException re) {
-			// ok!
-		}
-	}
+  @SuppressWarnings("static-method")
+  @Test
+  public void multipleCalls() {
+    try {
+      final LockOnce lo = new LockOnce();
+      Assert.assertTrue(lo.lockOnce()); // first one must be ok.
+      lo.lockOnce(); // second one makes no sense! it should fail quickly.
+      Assert
+      .fail("Second call to LockOnce.lockOnce() did not throw any Exception :-(");
+    } catch (final IllegalMonitorStateException re) {
+      // ok!
+    }
+  }
 
-	@Test
-	public void unlock() throws InterruptedException {
-		try {
-			final LockOnce lo = new LockOnce();
-			lo.unlock();
-			Assert.fail("LockOnce.unlock() did not throw any Exception :-(");
-		} catch (final IllegalMonitorStateException re) {
-			// ok!
-		}
+  @SuppressWarnings("static-method")
+  @Test
+  public void unlock() throws InterruptedException {
+    try {
+      final LockOnce lo = new LockOnce();
+      lo.unlock();
+      Assert.fail("LockOnce.unlock() did not throw any Exception :-(");
+    } catch (final IllegalMonitorStateException re) {
+      // ok!
+    }
 
-		{
-			final LockOnce lo = new LockOnce();
-			final AtomicBoolean check = new AtomicBoolean(false);
-			final Thread thread = new Thread() {
-				@Override
-				public void run() {
-					try {
-						lo.unlock();
-						Assert.fail("LockOnce.unlock() did not throw any Exception :-(");
-					} catch (final IllegalMonitorStateException re) {
-						// ok!
-						check.set(true);
-					}
-				}
-			};
-			thread.start();
-			thread.join();
-			Assert.assertTrue(check.get());
-		}
-	}
+    {
+      final LockOnce lo = new LockOnce();
+      final AtomicBoolean check2 = new AtomicBoolean(false);
+      final Thread thread = new Thread() {
+        @Override
+        public void run() {
+          try {
+            lo.unlock();
+            Assert.fail("LockOnce.unlock() did not throw any Exception :-(");
+          } catch (final IllegalMonitorStateException re) {
+            // ok!
+            check2.set(true);
+          }
+        }
+      };
+      thread.start();
+      thread.join();
+      Assert.assertTrue(check2.get());
+    }
+  }
 
+  static final class PseudoSingleton {
+    // All is volatile, final or synchronized. Visibility will be guaranteed by
+    // JVM.
+    final int foo1;
+    volatile int foo2;
+    private/* synchronized */int foo3;
 
-	static final class PseudoSingleton {
-		// All is volatile, final or synchronized. Visibility will be guaranteed by JVM.
-		final int																foo1;
-		volatile int														foo2;
-		private /*synchronized*/ int						foo3;
+    private static volatile PseudoSingleton instance = null;
 
-		private static volatile PseudoSingleton	instance	= null;
+    /** This is just here to give an example on how to use this correctly! */
+    public static PseudoSingleton getInstance() {
+      final LockOnce lo = new LockOnce();
+      if (lo.lockOnce())
+        try {
+          instance = new PseudoSingleton(
+              THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING);
+        } finally {
+          lo.unlock();
+        }
+      return instance;
+    }
 
-		/** This is just here to give an example on how to use this correctly! */
-		public static PseudoSingleton getInstance() {
-			final LockOnce lo = new LockOnce();
-			if (lo.lockOnce())
-				try {
-					instance = new PseudoSingleton(THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING);
-				} finally {
-					lo.unlock();
-				}
-			return instance;
-		}
+    /** This is just here to show how its done with a double checked lock! */
+    public static PseudoSingleton getInstanceDCL() {
+      if (instance == null)
+        synchronized (PseudoSingleton.class) {
+          if (instance == null)
+            instance = new PseudoSingleton(
+                THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING);
+        }
+      return instance;
+    }
 
-		/** This is just here to show how its done with a double checked lock! */
-		public static PseudoSingleton getInstanceDCL() {
-			if (instance == null)
-				synchronized (PseudoSingleton.class) {
-					if (instance == null)
-						instance = new PseudoSingleton(THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING);
-				}
-			return instance;
-		}
+    private static final class InstanceHolder {
+      public static PseudoSingleton instanceHolder = new PseudoSingleton(
+          THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING);
+    }
 
-		private static final class InstanceHolder {
-			public static PseudoSingleton	instance	= new PseudoSingleton(THE_ANSWER_TO_LIFE_THE_UNIVERSE_AND_EVERYTHING);
-		}
+    /** This is just here to show how its done with a nested class as a holder! */
+    public static PseudoSingleton getInstanceHolder() {
+      return InstanceHolder.instanceHolder;
+    }
 
-		/** This is just here to show how its done with a nested class as a holder! */
-		public static PseudoSingleton getInstanceHolder() {
-			return InstanceHolder.instance;
-		}
+    public PseudoSingleton(final int value) {
+      this.foo1 = value;
+    }
 
-		public PseudoSingleton(final int value) {
-			this.foo1 = value;
-		}
+    public synchronized int getFoo3() {
+      return this.foo3;
+    }
 
-		public synchronized int getFoo3() {
-			return this.foo3;
-		}
-
-		public synchronized void setFoo3(final int foo3) {
-			this.foo3 = foo3;
-		}
-	}
+    public synchronized void setFoo3(final int foo3) {
+      this.foo3 = foo3;
+    }
+  }
 }
